661dce35de0039c9505332b312e504229cef8ed0
Joey676 <Joe@JOESACER>
2018-08-06 19:50:24 +0100

    Rename Assembly

--- RLBotCSharpExample/Properties/AssemblyInfo.cs
+++ RLBotCSharpExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RLBotCSharpExample")]
+[assembly: AssemblyTitle("JoeyBot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RLBotCSharpExample")]
+[assembly: AssemblyProduct("JoeyBot")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
